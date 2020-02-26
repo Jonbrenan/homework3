a13dc07cad337c86759d0052149c2ecef8c2be9e
Jon Brenan <jonathanbrenan@gmail.com>
2020-02-26 14:18:52 -0500

    got a random password generator, but it is not complete yet

--- Develop/script.js
+++ Develop/script.js
@@ -9,22 +9,24 @@ var num = '0123456789';
 var sym = '!@#$%^&*=-_';
 
 
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
 
-
-
-let characters = ('char', 'charUpp')
-console.log(password(7, characters))
-
-function password(l,characters){
-		let pwd = '';
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  function generatePassword(l, characters){
+    let pwd = '';
     for(let i = 0; i<l; i++){
     		pwd += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return pwd;
+  }
+console.log(generatePassword(12,char))
+  passwordText.value = password;
+
 }
 
-
-
-
-
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
